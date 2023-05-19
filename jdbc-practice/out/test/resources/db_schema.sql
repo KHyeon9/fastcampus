26632d71ac7cb1e8b3ea1b94a83161e3346c15e2
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2023-05-19 17:00:45 +0900

    day-6
    [230519] jdbc 실습

---
+++ jdbc-practice/out/test/resources/db_schema.sql
@@ -0,0 +1,11 @@
+DROP Table IF EXISTS USERS;
+
+CREATE TABLE USERS (
+    userId varchar(20) NOT NULL,
+    password varchar(12) NOT NULL,
+    name varchar(20) NOT NULL,
+    email varchar(50),
+
+    PRIMARY KEY (userId)
+);
+
